cdeacd6de2fd7c17f9266a5db934555e318df860
fesily <fesil@foxmail.com>
2025-10-07 12:07:04 +0800

    increase max syntactic nesting level and local variables

--- src/lj_def.h
+++ src/lj_def.h
@@ -62,10 +62,10 @@ typedef unsigned int uintptr_t;
 #define LJ_MAX_COLOSIZE	16		/* Max. elems for colocated array. */
 
 #define LJ_MAX_LINE	LJ_MAX_MEM32	/* Max. source code line number. */
-#define LJ_MAX_XLEVEL	200		/* Max. syntactic nesting level. */
+#define LJ_MAX_XLEVEL	256		/* Max. syntactic nesting level. */
 #define LJ_MAX_BCINS	(1<<26)		/* Max. # of bytecode instructions. */
 #define LJ_MAX_SLOTS	250		/* Max. # of slots in a Lua func. */
-#define LJ_MAX_LOCVAR	200		/* Max. # of local variables. */
+#define LJ_MAX_LOCVAR	256		/* Max. # of local variables. */
 #define LJ_MAX_UPVAL	160		/* Max. # of upvalues. */
 
 #define LJ_MAX_IDXCHAIN	100		/* __index/__newindex chain limit. */
