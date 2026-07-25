01e10e28f55c09a94f558005ec0805bc7895d481
fesily <fesil@foxmail.com>
2026-07-25 15:16:33 +0800

    fix: define lj_assert_check no-op outside assert builds
    Release builds already stub lj_checkapi; provide lj_assert_check so
    call sites can use the same predicate without ifdef sprawl.

--- src/lj_def.h
+++ src/lj_def.h
@@ -358,6 +358,7 @@ static LJ_AINLINE uint32_t lj_getu32(const void *v)
    (lj_assert_fail((g), __FILE__, __LINE__, __func__, __VA_ARGS__), 0))
 #define lj_checkapi(c, ...)	lj_assert_check(G(L), (c), __VA_ARGS__)
 #else
+#define lj_assert_check(g, c, ...)	((void)0)
 #define lj_checkapi(c, ...)	((void)L)
 #endif
 
